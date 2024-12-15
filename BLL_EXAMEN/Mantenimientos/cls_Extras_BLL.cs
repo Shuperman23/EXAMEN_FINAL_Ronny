93a2b2415c02e4c2459ad6c9d194b5e5880234fe
ronny ruiz <roniruiz23@hotmail.com>
2024-12-15 17:35:29 -0600

    Extras

--- BLL_EXAMEN/Mantenimientos/cls_Extras_BLL.cs
+++ BLL_EXAMEN/Mantenimientos/cls_Extras_BLL.cs
@@ -17,11 +17,26 @@ namespace BLL_EXAMEN.Mantenimientos
             {
                 cls_BD_DAL Obj_BD_DAL = new cls_BD_DAL();
                 cls_BD_BLL Obj_BD_BLL = new cls_BD_BLL();
-
+                if (
+                  ((obj_Extras_DAL.iIdExtra == 0))
+                 )//listar
+                {
                 Obj_BD_DAL.sNomSP = ConfigurationManager.AppSettings["SP_LST_Extras"];
                 Obj_BD_DAL.DT_Parametros = null;
                 Obj_BD_DAL.sNomTabla = "Extras";
+                }
+                else //filtrar
+                {
+                    obj_Extras_DAL.dtParametros = null;
+                    obj_Extras_DAL.dtParametros = Obj_BD_BLL.ObtieneDTParametros(obj_Extras_DAL.dtParametros);
 
+                    //orden de parametros: nombre  , tipo de dato, valor del parametro
+                    obj_Extras_DAL.dtParametros.Rows.Add("@Extra", "1", obj_Extras_DAL.iIdExtra);
+
+                    Obj_BD_DAL.sNomSP = ConfigurationManager.AppSettings["SP_FILL_Sucursales"];
+                    Obj_BD_DAL.DT_Parametros = obj_Extras_DAL.dtParametros;
+                    Obj_BD_DAL.sNomTabla = "Sucursales";
+                }
                 Obj_BD_BLL.EjecutaProcesosTabla(ref Obj_BD_DAL);
 
                 if (Obj_BD_DAL.sMsjErrorBD == string.Empty)
